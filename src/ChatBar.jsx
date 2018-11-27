0d674115b04d1a5cd34e10d1b3f65a892c36ad26
SylvainJunca <sylvain.junca@gmail.com>
2018-11-27 14:15:46 -0500

    ChatBatr component now takes a function from app and sends back value to feed the messages list !!

--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -1,28 +1,21 @@
 import React, { Component} from 'react';
 
 export default class ChatBar extends Component {
+  handleInputMessage = (e)  => {
+    if(e.keyCode == 13){
+      console.log(e.target.value);
+      this.props.addMessage(e.target.value);
+      e.target.value = '';
+    }
+  }
+
   render (){ 
-    if(this.props.user){
     return(
       <footer className="chatbar">
-        <input className="chatbar-username" defaultValue={this.props.user.name} />
-        <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
+        <input className="chatbar-username" defaultValue={this.props.user ? this.props.user.name : "Your name (Optional)"} />
+        <input onKeyUp={this.handleInputMessage} className="chatbar-message" placeholder="Type a message and hit ENTER" />
     </footer>)
 
-  }
-   return(
-    <footer className="chatbar">
-      <input className="chatbar-username" placeholder="Your Name (Optional)" />
-      <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
-  </footer>
-   );
-  }
-} 
-
-
-
-
-
-
-
-
+  
+  } 
+}
