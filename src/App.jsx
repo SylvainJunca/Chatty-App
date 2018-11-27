881662a5f4b5c467baafa33692df4bb82a547ac3
SylvainJunca <sylvain.junca@gmail.com>
2018-11-26 20:31:10 -0500

    Creates the files and links to build our app in React

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import React, {Component} from 'react';
+import MessageList from './MessageList.jsx';
+
 
 class App extends Component {
   render() {
     return (
-      <h1>Hello React :)</h1>
+      <div>
+        <nav className="navbar">
+          <a href="/" className="navbar-brand">Chatty</a>
+        </nav>
+        <MessageList />
+          <footer className="chatbar">
+            <input className="chatbar-username" placeholder="Your Name (Optional)" />
+            <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
+          </footer>
+      </div>
     );
   }
 }
