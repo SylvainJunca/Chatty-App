14c82e2f176b403f7ff9b01d2e2a86b00ba3ebe2
SylvainJunca <sylvain.junca@gmail.com>
2018-11-27 16:27:51 -0500

    installs the websocket dependencies and creates the server.js file

--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -1,8 +1,12 @@
 import React, { Component} from 'react';
 
 export default class ChatBar extends Component {
+
+  // This function checks if the Key up is 'Enter'
+  // If it's the case it takes the value of the input field
+  // and triggers the addMessage function inherited from  App component
   handleInputMessage = (e)  => {
-    if(e.keyCode == 13){
+    if(e.keyCode == 13){ 
       console.log(e.target.value);
       this.props.addMessage(e.target.value);
       e.target.value = '';
